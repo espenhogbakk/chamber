3683afc08fff3a943f7d878808eb3a6d0ef4bfe9
Espen Høgbakk <espen@hogbakk.no>
2012-02-04 21:38:15 +0100

    Add redis url

--- app/observers/juggernaut_observer.rb
+++ app/observers/juggernaut_observer.rb
@@ -15,7 +15,7 @@ class JuggernautObserver < ActiveRecord::Observer
   
   protected
     def publish(type, rec)
-      #Juggernaut.url = 'someredisurl'
+      Juggernaut.url = 'redis://redistogo:3d4284db411dad03463355e7e48f92f5@viperfish.redistogo.com:9158/'
       Juggernaut.publish("/observer", {
         :type  => type, :id => rec.id, 
         :klass => rec.class.name, :record => rec
